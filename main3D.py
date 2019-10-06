18fa157c57965077edb6557c40abdb67381f1f02
murat.cankaya <43417789+Iammuratc@users.noreply.github.com>
2019-10-06 14:17:00 +0200

    Add files via upload

--- main3D.py
+++ main3D.py
@@ -1,70 +1,67 @@
-from unet_master.model import *
-from unet_master.data import *
+from model import *
+from data import *
 from keras.utils import plot_model
 import matplotlib.pyplot as plt
 
-from unet_master.testGenerator import timeSeriesGenerator, timeSeriesGenerator3D
+# from unet_master.testGenerator import timeSeriesGenerator, timeSeriesGenerator3D
 
+import scipy.misc
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-from unet_master.write3Dimage import write3D
+from write3Dimage import write3D
+import os
+import numpy as np
+import scipy.io
+import h5py
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-path='D:\\Uni\\AlCu\\'
+netname='unet3D_weights.hdf5'
 
-netname='unet3D.hdf5'
+## Training
+
 kernel=3
-timesteps=1;
-datasize=64-16
 datasize=64
 scale=2
-gen=timeSeriesGenerator3D(1,path,[20,39],[160,160,30], [760,760,600],target_size=(datasize,datasize,datasize),scale=scale)
-model = unet3D(input_size = (datasize,datasize,datasize,1),kernelSize=kernel)
-model.load_weights(netname)
+# gen=timeSeriesGenerator3D(1,path,[20,39],[160,160,30], [760,760,600],target_size=(datasize,datasize,datasize),scale=scale)
+# model = unet3D(input_size = (datasize,datasize,datasize,1),kernelSize=kernel)
+# model.load_weights(netname)
 
-model.summary()
-model_checkpoint = ModelCheckpoint(netname, monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(gen,steps_per_epoch=300,epochs=20,callbacks=[model_checkpoint])
-
-
+# model.summary()
+# model_checkpoint = ModelCheckpoint(netname, monitor='loss',verbose=1, save_best_only=True)
+# model.fit_generator(gen,steps_per_epoch=300,epochs=20,callbacks=[model_checkpoint])
 
 
-time=65
+
+## Testing
+
+data_path='C://Users//Murat//PycharmProjects//unet-thesis//thesis_data//low_temp//source'
+
+mat='ct_normalized_t2.mat'
+arrays = {}
+f = h5py.File(os.path.join(data_path,mat))
+a = list(f['normalized_imgs_backup'])
+images= np.transpose(a) / (256.0*256.0-1)
+print('input max: {}'.format(np.max(images[:,:,550])))
+
 datasize=128
 
-#testimage=np.zeros(shape=(1,scale*datasize,scale*datasize,scale*datasize,1))
 testimage=np.zeros(shape=(scale*datasize,scale*datasize,scale*datasize))
-testpath='D:\\Daten_Projekte\\TessellationZeitreihe\\Data\\probe11_b__'
+
 model = unet3D(input_size = (datasize,datasize,datasize,1),kernelSize=kernel)
 model.load_weights(netname)
 start=501
-for t in range(start,start+datasize*scale):
-    im = io.imread(
-        path + 'probe11_b__' + format(time, '03d') + '\\0-ring_correction_cutout\\slice_' +
-        format(t, '04d') + '.tif', as_gray=True) / 255.0
-    tmp=np.zeros(shape=(scale*datasize,scale*datasize))
-    tmp[0:np.shape(im)[0],0:np.shape(im)[1]]=im[181:181+datasize*scale,181:181+datasize*scale]
-    #tmp=im[201:201+datasize,201:201+datasize]
-    testimage[:,:,t-start]=tmp
+
+testimage=images[181:181+datasize*scale,181:181+datasize*scale,start:start+datasize*scale]
+
+outpath=os.path.join(os.getcwd(),'data\\test_input')
+write3D(testimage,outpath)
 
 image=np.zeros(shape=(1,datasize,datasize,datasize,1))
-outpath='D:\\Uni\\AlCu\\probe11_b__045\\test2'
-write3D(testimage,outpath)
 image[0,:,:,:,0]=trans.resize(testimage,[datasize,datasize,datasize])
 testimage=0
 
 out=model.predict(image)
 out=out[0,:,:,:,0]
 
-fig,axs=plt.subplots(nrows=1,ncols=3, sharex=True, figsize=(3, 5))
-axs[0].imshow(out[:,:,32],cmap='gray')
-plt.show()
-
-outpath='D:\\Uni\\AlCu\\probe11_b__045\\test'
+outpath=os.path.join(os.getcwd(),'data\\test_output')
 write3D(out, outpath, normalization=255.0)
-
-#
-# for t in range(-timesteps,timesteps+1):
-#     plt.figure()
-#     plt.imshow(testimage[0,:,:,t+timesteps])
-# plt.show()
--- testGenerator.py
+++ testGenerator.py
@@ -7,52 +7,10 @@ import matplotlib.pyplot as plt
 import skimage.transform as trans
 
 
-
-
-def timeSeriesGenerator(batch_size,path,time_area,image_low,image_high, time_steps=1,target_size=(256,256),seed=1,normalization=255.0):
-
-    lowTime=time_area[0];
-    highTime=time_area[1]-2*time_steps-1;
-    low= image_low #+ [scipy.math.ceil(target_size / 2),time_steps]
-
-   # high=np.array(image_high)- np.array([target_size[0],target_size[1], 2*time_steps+1])
-    high=np.array(image_high)- np.array([target_size[0],target_size[1], 0])
-
-
-
-    #for i in range(1, batch_size+1):
-    while True:
-        input=np.zeros(shape=(batch_size,target_size[0],target_size[1],2*time_steps+1))
-        labels=np.zeros(shape=(batch_size,target_size[0],target_size[1],1))
-        for i in range(0, batch_size ):
-            t=randint(lowTime, lowTime)
-            corner=[randint(low[0],high[0]),randint(low[1],high[1]),randint(low[2],high[2])]
-
-
-            label=np.zeros(shape=(target_size[0],target_size[1]))
-            for time in range(0, 2*time_steps+1):
-                im = io.imread(
-                    path + 'probe11_b__' + format(t+time, '03d') + '\\0-ring_correction_cutout\\slice_' +
-                    format(corner[2],'04d') + '.tif',as_gray=True)/normalization
-
-                input[i,:,:,time]=im[corner[0]: corner[0]+target_size[0],corner[1]: corner[1]+target_size[1] ]
-
-            labelLarge=io.imread(
-                    path + 'probe11_b__' + format(t+time, '03d') + '\\3-regions\\slice_' +
-                    format(corner[2],'04d') + '.tif',as_gray=True)/normalization
-            label=labelLarge[corner[0]: corner[0]+target_size[0],corner[1]: corner[1]+target_size[1] ]
-            #out[0:time_steps,i,:,:]=images
-            labels[i,:,:,0]=label
-
-
-        #print('shape')
-        #print(shape((out,labels)))
-        yield input,labels#(images,label)
-
 def timeSeriesGenerator3D(batch_size,path,time_area,image_low,image_high,target_size=(256,256),scale=1,seed=1,normalization=255.0):
 
-    lowTime=time_area[0];
-    highTime=time_area[1];
+    lowTime=time_area[0]
+    highTime=time_area[1]
     low= image_low #+ [scipy.math.ceil(target_size / 2),time_steps]
 
    # high=np.array(image_high)- np.array([target_size[0],target_size[1], 2*time_steps+1])
@@ -91,25 +49,3 @@ def timeSeriesGenerator3D(batch_size,path,time_area,image_low,image_high,target_
 
 
         yield input,labels#(images,label)
-#
-#
-# path='D:\\Daten_Projekte\\TessellationZeitreihe\\Data\\'
-# time_area=[20,75]
-# time_steps=1
-# lowTime=time_area[0]+time_steps;
-# highTime=time_area[1]-time_steps;
-# print(path+'probe11_b__'+format(lowTime, '03d')+'\\0-ring_correction_cutout\\slice_'+format(74,'04d')+ '.tif' )
-# im=io.imread(path+'probe11_b__'+format(lowTime, '03d')+'\\0-ring_correction_cutout\\slice_'+format(74,'04d')+ '.tif',as_gray = True)
-# gen=timeSeriesGenerator(5,path,[20,39],[160,160,30], [660,660,400])
-# for i in range(1, 10+1):
-#     print(i)
-#     X,Y=gen.__next__()
-#     print(np.shape(X))
-# fig,axs=plt.subplots(nrows=2,ncols=2, sharex=True, figsize=(3, 5))
-# axs[0][0].imshow(X[0][:,:,0],cmap='gray')
-# axs[0][1].imshow(X[0][:,:,1],cmap='gray')
-# axs[1][0].imshow(X[0][:,:,2],cmap='gray')
-# axs[1][1].imshow(X[1],cmap='gray')
-# plt.figure()
-# plt.imshow(X[0][:,:,2],cmap='gray')
-# plt.show()
